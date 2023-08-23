c7f654a876d7f2e787dad3cafee10c8d7b7c8101
BasujitaBhattacharya <121059306+BasujitaBhattacharya@users.noreply.github.com>
2023-08-23 16:16:41 -0400

    Update sample-1-demo.java

--- examples/sample-1-demo.java
+++ examples/sample-1-demo.java
@@ -1,1 +1,20 @@
-
+/**
+ * The Foo class is a simple example to illustrate documentation 
+ * comments.
+ */
+public class Foo { 
+    /**
+    * An integer to keep track of for fun.
+    */
+    private int count; 
+    ... 
+    /**
+    * Increment a value by delta and return the new value. 
+    *
+    * @param  delta   the amount the value should be incremented by
+    * @return         the post-incremented value
+    */
+   int increment(int delta) {
+       ...
+   }
+} 
