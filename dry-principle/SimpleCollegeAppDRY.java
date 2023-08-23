a2d76b6dba0b8d2f112a9e31f9f48e087336c96e
BasujitaBhattacharya <121059306+BasujitaBhattacharya@users.noreply.github.com>
2023-08-23 18:33:51 -0400

    Update SimpleCollegeAppDRY.java

--- dry-principle/SimpleCollegeAppDRY.java
+++ dry-principle/SimpleCollegeAppDRY.java
@@ -1,64 +1,64 @@
-import java.util.*;
- 
-// Main class
-public class SimpleCollegeAppDRY {
- 
-    // Method 1
-    // For cse department
-    public void CSE()
-    {
- 
-        // Print statement
-        System.out.println("This is computer science");
- 
-        // Calling method
-        college();
-    }
- 
-    // Method 2
-    // For ece dept method
-    public void ECE()
-    {
-        System.out.println("This is electronics");
- 
-        // Calling method
-        college();
-    }
- 
-    // Method 3
-    // For IT dept
-    public void IT()
-    {
- 
-        // Print statement
-        System.out.println(
-            "This is Information Technology");
- 
-        // Calling method
-        college();
-    }
- 
-    // Method 4
-    // For college dept
-    public void college()
-    {
- 
-        // Print statement
-        System.out.println("IIT - Madras");
-    }
- 
-    // Method 5
-    // Main driver method
-    public static void main(String[] args)
-    {
- 
-        // Creating object of class in main() method
-        GFG s = new GFG();
- 
-        // Calling the methods one by one
-        // as created above
-        s.CSE();
-        s.ECE();
-        s.IT();
-    }
-}
+   import java.util.*;
+    
+   // Main class
+   public class SimpleCollegeAppDRY {
+    
+       // Method 1
+       // For cse department
+       public void CSE()
+       {
+    
+           // Print statement
+           System.out.println("This is computer science");
+    
+           // Calling method
+           college();
+       }
+    
+       // Method 2
+       // For ece dept method
+       public void ECE()
+       {
+           System.out.println("This is electronics");
+    
+           // Calling method
+           college();
+       }
+    
+       // Method 3
+       // For IT dept
+       public void IT()
+       {
+    
+           // Print statement
+           System.out.println(
+               "This is Information Technology");
+    
+           // Calling method
+           college();
+       }
+    
+       // Method 4
+       // For college dept
+       public void college()
+       {
+    
+           // Print statement
+           System.out.println("IIT - Madras");
+       }
+    
+       // Method 5
+       // Main driver method
+       public static void main(String[] args)
+       {
+    
+           // Creating object of class in main() method
+           GFG s = new GFG();
+    
+           // Calling the methods one by one
+           // as created above
+           s.CSE();
+           s.ECE();
+           s.IT();
+       }
+   }
