d241946f27e4c154f59261e9a93cc96251122961
BasujitaBhattacharya <121059306+BasujitaBhattacharya@users.noreply.github.com>
2023-08-24 16:47:58 -0400

    Update Subject.java

--- examples/Subject.java
+++ examples/Subject.java
@@ -13,3 +13,8 @@ class Topic {
         System.out.println("Tight coupling concept");
     }
 }
+
+/*In the above program the Subject class is dependents on Topic class. 
+In the above program Subject class is tightly coupled with Topic class it means if any change in the Topic class requires Subject class to change.
+For example, if Topic class understand() method change to gotit() method then you have to change the startReading() method will call gotit() method instead of calling understand() method.
+*/
