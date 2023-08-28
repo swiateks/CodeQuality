b27ed4566f7cc8cd7b36cdc9affb268d252b02c8
Basujita Bhattacharya <basujitab@microsoft.com>
2023-08-28 10:13:51 -0400

    Changing Directory Structure

--- codeReview/Database.java
+++
@@ -1,9 +0,0 @@
-public class Database
-{
-    private SqlServerConnection _databaseConnection;
-
-    public Database(SqlServerConnection databaseConnection)
-    {
-        _databaseConnection = databaseConnection;
-    }
-}
